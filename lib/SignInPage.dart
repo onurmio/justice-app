9888df2881f51487f554fa5b75df591aee518e7d
umurbeytaskin <umurbeytaskinn@gmail.com>
2021-03-06 00:51:24 +0300

    initial commit

---
+++ lib/SignInPage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body(),
+    );
+  }
+
+  Widget _body(){
+
+    return Container(
+
+        color: Color(0XFF2387C1),
+
+    );
+
+  }
+
+}
+
